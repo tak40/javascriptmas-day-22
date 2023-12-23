74825213c631e11451e5badcce7c659723574212
Tak <takeshiseodev@gmail.com>
2023-12-23 14:54:17 -0500

    update:

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
-let people = []
+/** @format */
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const peopleListEl = document.getElementById("people-list")
+let people = JSON.parse(localStorage.getItem('peopleList')) || []
 
-addButtonEl.addEventListener("click", function() {
+const inputFieldEl = document.getElementById('input-field')
+const addButtonEl = document.getElementById('add-button')
+const peopleListEl = document.getElementById('people-list')
+
+addButtonEl.addEventListener('click', function () {
     let inputValue = inputFieldEl.value
-    
-    if (inputValue) {
+
+    if (inputValue && !people.includes(inputValue)) {
         people.push(inputValue)
-        
+
+        localStorage.setItem('peopleList', JSON.stringify(people))
+
         clearInputFieldEl()
-        
+
         renderList(people)
+    } else {
+        alert('Please enter a unique name or a non-empty value.')
     }
 })
 
 function renderList(array) {
     clearPeopleListEl()
-    
+
     for (let i = 0; i < array.length; i++) {
         let currentPerson = array[i]
-        
+
         appendPersonToPeopleListEl(currentPerson)
     }
 }
 
 function clearPeopleListEl() {
-    peopleListEl.innerHTML = ""
+    peopleListEl.innerHTML = ''
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+    inputFieldEl.value = ''
 }
 
 function appendPersonToPeopleListEl(person) {
-    
-    let newEl = document.createElement("li")
-    
+    let newEl = document.createElement('li')
+
     newEl.textContent = person
-    
-    newEl.addEventListener("dblclick", function() {
+
+    newEl.addEventListener('dblclick', function () {
         let index = people.indexOf(person)
 
-        people.splice(index, 1)
-            
+        if (index > -1) {
+            people.splice(index, 1)
+        }
+
+        localStorage.setItem('peopleList', JSON.stringify(people))
+
         renderList(people)
+
+        replaceImageWithGif()
     })
-    
+
     peopleListEl.append(newEl)
 }
+
+function replaceImageWithGif() {
+    let imgEl = document.querySelector('img')
+    if (imgEl) {
+        imgEl.src = 'https://media.giphy.com/media/hZj44bR9FVI3K/giphy.gif'
+    }
+}
+
+renderList(people)
